fd76e9e2f8e337644a042089336ce695f64ca24a
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-06-26 15:14:05 +0200

    Refactored the simple command bus builder
    - Added better documentation for the simple bus builder
    - Added design by contract preconditions to ensure the builder's integrity
    - Unfolded the chained if conditionals into a single-level if to prevent confusion

--- commandbus-core/src/main/java/dev/sergheev/commandbus/SimpleCommandBusBuilder.java
+++ commandbus-core/src/main/java/dev/sergheev/commandbus/SimpleCommandBusBuilder.java
@@ -7,12 +7,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import static java.util.Objects.requireNonNull;
+
 /**
- * A simple fluent API for the construction of {@link SimpleCommandBus}.
+ * A simple fluent API for the construction of a {@link SimpleCommandBus}.
  *
- * <p>The class is responsible for the instantiation of {@link SimpleCommandBus}
- * upon given preferences like {@link CommandHandler} pre-registration in the
- * container, or if the bus must be concurrent or not.</p>
+ * <p>This object is responsible for the instantiation of a {@link SimpleCommandBus},
+ * it allows to easily construct the bus and specify custom properties that will
+ * define the bus behavior at runtime (i.e. thread-safety).
  */
 @SuppressWarnings({ "rawtypes" })
 public class SimpleCommandBusBuilder {
@@ -31,38 +33,61 @@ public class SimpleCommandBusBuilder {
         this.classToInstance = new HashMap<>();
     }
 
+    /**
+     * Ensures the thread-safety of the constructed bus and its functioning in multithreaded environments.
+     * @return the current builder
+     */
     public SimpleCommandBusBuilder concurrent() {
         isConcurrent = true;
         return this;
     }
 
+    /**
+     * Registers a new handler that will contain behavior that is to be associated with a command.
+     * @param type the type of the command handler object that is to be registered
+     * @param instance the instance that matches the previously specified type
+     * @throws IllegalArgumentException if the instance does not match the given type
+     * @return the current builder
+     */
     public SimpleCommandBusBuilder registerHandler(Class<? extends CommandHandler> type, Object instance) {
+        if(!type.isInstance(instance)) throw new IllegalArgumentException("The given instance must match the type");
         classToInstance.put(type, instance);
         return this;
     }
 
+    /**
+     * Registers multiple new handlers that will contain behavior that is to be associated with their commands.
+     * @param handlers a map containing the type-instance handler associations
+     * @throws IllegalArgumentException if any instance in the given map does not match its type
+     * @return the current builder
+     */
     public SimpleCommandBusBuilder registerHandlers(Map<Class<? extends CommandHandler>, Object> handlers) {
-        handlers.forEach(classToInstance::put);
+        handlers.forEach(this::registerHandler);
         return this;
     }
 
+    /**
+     * Sets a manually managed or custom handler registry instance that is to be used in this bus.
+     * @param customRegistry the custom command handler registry instance
+     * @return the current builder
+     */
     public SimpleCommandBusBuilder withRegistry(CommandHandlerRegistry customRegistry) {
-        this.customRegistry = customRegistry;
+        this.customRegistry = requireNonNull(customRegistry, "customRegistry");
         return this;
     }
 
     public SimpleCommandBus build() {
         CommandHandlerRegistry handlerRegistry;
-        boolean hasNotCustomRegistry = Objects.isNull(customRegistry);
-        if(hasNotCustomRegistry) {
-            if(isConcurrent) handlerRegistry = CommandHandlerRegistryFactory.newConcurrentRegistry();
-            else handlerRegistry = CommandHandlerRegistryFactory.newRegistry();
-        } else {
+        boolean hasCustomRegistry = !Objects.isNull(customRegistry);
+        if(hasCustomRegistry) {
             handlerRegistry = customRegistry;
+        } else if(isConcurrent) {
+            handlerRegistry = CommandHandlerRegistryFactory.newConcurrentRegistry();
+        } else {
+            handlerRegistry = CommandHandlerRegistryFactory.newRegistry();
         }
         classToInstance.forEach(handlerRegistry::registerHandler);
         CommandHandlerFinder commandHandlerFinder = new SimpleCommandHandlerFinder(handlerRegistry);
-
         return new SimpleCommandBus(commandHandlerFinder);
     }
 
